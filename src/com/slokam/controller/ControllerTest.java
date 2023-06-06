ad9fc4cdee2d0f2a6a7e361ca19c8bbecca49152
phanikumar <phanikumarnspk@gmail.com>
2023-06-06 11:46:35 +0530

    gitproject testing

---
+++ src/com/slokam/controller/ControllerTest.java
@@ -0,0 +1,13 @@
+package com.slokam.controller;
+
+public class ControllerTest {
+
+	public void Test1() {
+		System.out.println("Test1()");
+	}
+	
+	public static void main(String[] args) {
+		ControllerTest controllerTest = new ControllerTest();
+		controllerTest.Test1();
+	}
+}
